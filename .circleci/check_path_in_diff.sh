8a235b2eec5589d996974b4b788275ad8517546b
val <8445610+valmack@users.noreply.github.com>
2020-10-16 07:08:32 -0400

    feat: run ci jobs when dockerfiles change
    fix: checkout and use sh in job
    
    fix: remove sh
    
    fix: add shebang to script
    
    feat: set error flags in bash script
    
    feat: add shellcheck orb
    
    fix: remove circleci folder from script path

---
+++ .circleci/check_path_in_diff.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+set -o nounset
+set -o errexit
+set -o pipefail
+
+FILE=$1
+
+LATEST_COMMIT=$(git rev-parse HEAD)
+
+LATEST_FILE_COMMIT=$(git log -1 --format=format:%H --full-diff $FILE)
+
+if [ $LATEST_FILE_COMMIT = $LATEST_COMMIT ];
+    then
+        echo "File $FILE changed in latest commit"
+else
+    echo "No change to file $FILE in latest commit"
+    exit 0;
+fi
