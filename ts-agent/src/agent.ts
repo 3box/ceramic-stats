5f02c9b4fe874af0f85c6ae65f84d798304dddd4
Golda Velez <gv@birdbox.local>
2022-06-09 01:37:53 -0700

    start pulling in ceramic packages

--- ts-agent/src/agent.ts
+++ ts-agent/src/agent.ts
@@ -1,2 +1,9 @@
+import { CID } from 'multiformats/cid'
+import { IpfsDaemon } from '@ceramicnetwork/ipfs-daemon'
+import dagJose from 'dag-jose'
+
+const CERAMIC_NETWORK = process.env.CERAMIC_NETWORK || 'dev-unstable'
+const { IPFS_API_URL } = process.env
+const IPFS_PUBSUB_TOPIC = process.env.IPFS_PUBSUB_TOPIC || '/ceramic/dev-unstable'
 
 console.log("hi")
