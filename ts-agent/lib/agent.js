5af0acd1181ace16d13457f140449f396ca8505d
Golda Velez <gv@birdbox.local>
2022-06-09 14:25:05 -0700

    migrating code to typescript

--- ts-agent/lib/agent.js
+++
@@ -1,2 +0,0 @@
-console.log("hi");
-//# sourceMappingURL=agent.js.map
--- ts-agent/src/agent.ts
+++ ts-agent/src/agent.ts
@@ -1,9 +1,51 @@
 import { CID } from 'multiformats/cid'
 import { IpfsDaemon } from '@ceramicnetwork/ipfs-daemon'
+import IpfsHttpClient from 'ipfs-http-client'
+import convert from 'blockcodec-to-ipld-format'
 import dagJose from 'dag-jose'
+import path from "path"
+import debug from 'debug'
 
 const CERAMIC_NETWORK = process.env.CERAMIC_NETWORK || 'dev-unstable'
-const { IPFS_API_URL } = process.env
+const IPFS_API_URL = process.env.IPFS_API_URL || 'http://localhost:5001'
 const IPFS_PUBSUB_TOPIC = process.env.IPFS_PUBSUB_TOPIC || '/ceramic/dev-unstable'
 
-console.log("hi")
+const error = debug('ceramic:agent:error')
+const log = debug('ceramic:agent:log')
+log.log = console.log.bind(console)
+
+log("hi")
+let ipfs
+
+async function main() {
+    ipfs = await createIpfs(IPFS_API_URL)
+    await ipfs.pubsub.subscribe(IPFS_PUBSUB_TOPIC, handleMessage)
+    log('Subscribed to pubsub topic', IPFS_PUBSUB_TOPIC)
+    log('Ready')
+}
+
+/**
+ * Returns IPFS instance. Given url, uses ipfs http client.
+ *
+ * NOTE: IPFS nodes are not designed for multi-client usage. When using pubsub,
+ * the IPFS url should not be shared between multiple clients.
+ */
+async function createIpfs(url) {
+    let ipfs
+
+    log('Starting ipfs...')
+    if (url) {
+        const dagJoseFormat = convert.convert(dagJose)
+        const ipld = { formats: [dagJoseFormat] }
+        ipfs = await IpfsHttpClient.create(url, ipld)
+    } else {
+        const config = {
+            ipfsPath: path.join(__dirname, '../ipfs'),
+            ceramicNetwork: CERAMIC_NETWORK
+        }
+        let ipfsDaemon = await IpfsDaemon.create(config)
+        await ipfsDaemon.start()
+        ipfs = ipfsDaemon.ipfsd
+    }
+    return ipfs
+}
