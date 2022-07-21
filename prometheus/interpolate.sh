460dbec69c34bc2a8f7e2a38324ce7da4305d479
Golda Velez <golda@3box.io>
2022-07-21 14:43:50 +0200

    feat: insert env vars into prometheus config file

--- prometheus/interpolate.sh
+++ prometheus/interpolate.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-#cd /etc/prometheus
-echo "dum"
+sed -i -e "s/%%CERAMIC_TARGET%%/$CERAMIC_METRICS_HOST:$METRICS_EXPORTER_PORT/" /etc/prometheus/prometheus.yml
+sed -i -e "s/%%CAS_TARGET%%/$CAS_METRICS_HOST:$METRICS_EXPORTER_PORT/" /etc/prometheus/prometheus.yml
 
-#sed -i '' -e "s/%%CERAMIC_TARGET%%/$CERAMIC_METRICS_HOST:$METRICS_EXPORTER_PORT/" prometheus.yml
-#sed -i '' -e "s/%%CAS_TARGET%%/$CAS_METRICS_HOST:$METRICS_EXPORTER_PORT/" prometheus.yml
