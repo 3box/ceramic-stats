190b51503ac42d4efc4b3c1511dbc72c09f1b170
Golda Velez <golda@3box.io>
2022-07-21 14:01:04 +0200

    squashme - check in debugging version

---
+++ prometheus/interpolate.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#cd /etc/prometheus
+echo "dum"
+
+#sed -i '' -e "s/%%CERAMIC_TARGET%%/$CERAMIC_METRICS_HOST:$METRICS_EXPORTER_PORT/" prometheus.yml
+#sed -i '' -e "s/%%CAS_TARGET%%/$CAS_METRICS_HOST:$METRICS_EXPORTER_PORT/" prometheus.yml
