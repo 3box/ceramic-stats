0ed629527c3a2bd819a06001df13c8826a986d23
Golda Velez <golda@3box.io>
2023-05-16 19:04:03 -0700

    typo

--- promscale/dobuild-prod.sh
+++ promscale/dobuild-prod.sh
@@ -1,4 +1,4 @@
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 967314784947.dkr.ecr.us-east-2.amazonaws.com
 docker build -t ceramic-prod-promscale .
-docker tag:ceramic-prod-promscale:latest 967314784947.dkr.ecr.us-east-2.amazonaws.com/ceramic-prod-promscale:prod
+docker tag ceramic-prod-promscale:latest 967314784947.dkr.ecr.us-east-2.amazonaws.com/ceramic-prod-promscale:prod
 docker push 967314784947.dkr.ecr.us-east-2.amazonaws.com/ceramic-prod-promscale:prod
