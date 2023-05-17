eab733f7c595e989902bee39e5265c1aa1620d2c
Golda Velez <golda@3box.io>
2023-05-17 09:53:43 -0700

    here is a thing

--- grafana/src/sync-cloud.py
+++ grafana/src/sync-cloud.py
@@ -2,15 +2,18 @@ import os
 import json
 
 FOLDERS = {
-    'Adoption' : {'dev': (130,'3cfiaAD4k'),
+    'Adoption' : {'prod': (20, '6YHYZKvVk'),
+                  'dev': (130,'3cfiaAD4k'),
                   'clay': (37,'fppmip5Vz'),
                   'qa': (50, '-E-Amt5Vz'),
                 },
-    'Product': {'dev': (135,'7S9sC554k'),
+    'Product': {  'prod': (17, 'nHWxziD4k'),
+                  'dev': (135,'7S9sC554k'),
                   'clay': (39,'dFjZmt54k'),
                   'qa': (51, '0oo1itc4k'),
                 },
-    'Performance': { 'dev': (136,'gNKUj55Vz'),
+    'Performance': { 'prod': (31, 'JXA6aUKVk'), 
+                  'dev': (136,'gNKUj55Vz'),
                   'clay': (38,'FFQiipc4z'),
                   'qa': (49, 'BXDAmt54k'),
                 },
@@ -67,13 +70,31 @@ BOARDS =  {
 
 GET_CMD = 'curl -X GET --insecure -H "Authorization: Bearer ' + os.environ.get('CERAMIC_GRAFANA_API_KEY') + '" -H "Content-Type: application/json" https://ceramic-stats.3boxlabs.com/api/dashboards/uid/{}'
 
-tokens = {
-  'dev' : os.environ.get('CERAMIC_DEV_GRAFANA_API_KEY'),
-  'clay': os.environ.get('CERAMIC_CLAY_GRAFANA_API_KEY'),
-  'qa': os.environ.get('CERAMIC_QA_GRAFANA_API_KEY'),
-}
+cloud_token = os.environ.get('CLOUD_GRAFANA_API_KEY')
 
-POST_CMD = 'curl -X POST --insecure -H "Authorization: Bearer {}" -H "Content-Type: application/json" --data-binary "@/tmp/dash" https://ceramic-stats-{}.3boxlabs.com/api/dashboards/db'
+POST_CMD = 'curl -X POST --insecure -H "Authorization: Bearer {}" -H "Content-Type: application/json" --data-binary "@/tmp/dash" https://threebox.grafana.net/api/dashboards/db'
+
+GET_CLOUD_FOLDER = 'curl -X GET --insecure -H "Authorization: Bearer ' + os.environ.get('CLOUD_GRAFANA_API_KEY') + '" -H "Content-Type: application/json" https://threebox.grafana.net/api/folders/{}'
+
+POST_CLOUD_FOLDER = 'curl -X POST --insecure -H "Authorization: Bearer ' + os.environ.get('CLOUD_GRAFANA_API_KEY') + '" -H "Content-Type: application/json" --data \'{}\' https://threebox.grafana.net/api/folders' 
+
+def fix_folder_stuff(b_data, cloud_folder, cloud_folder_uid):
+    try:
+      del b_data['meta']['folderId']
+    except:
+      print("No folder id for " + cloud_folder)
+    b_data["meta"]["folderTitle"] = cloud_folder 
+    b_data["meta"]["folderUrl"] = "/dashboards/f/{}/{}".format(cloud_folder_uid, cloud_folder.lower())
+    b_data['folderUid'] = cloud_folder_uid 
+
+    # check if folder exists, create if not
+    cmd = GET_CLOUD_FOLDER.format(cloud_folder_uid)
+    res = json.loads(os.popen(cmd).read())
+    if res.get('status','') == 'not-found': 
+       folder_data = {'uid': cloud_folder_uid, 'title': cloud_folder}
+       create_cmd = POST_CLOUD_FOLDER.format(json.dumps(folder_data))
+       os.system(create_cmd)
+
 
 for (name, boards_data) in BOARDS.items():
     print("On dashboard " + name)
@@ -86,19 +107,25 @@ for (name, boards_data) in BOARDS.items():
     with open("./static-backup/{}".format(name), 'w') as dashfile:
        json.dump(b_data, dashfile, indent=4)
 
+    b_folder = boards_data['folder']    
+
+
     for env in boards_data.keys():
-        if (env == 'prod'):
-            continue
         if (env == 'folder'):
             continue
         print("\nOn env " + env)
+        cloud_folder = b_folder
+        (f_id, f_uid) = FOLDERS[b_folder][env]
+
+        if env != 'prod':
+            cloud_folder += '-' + env
+
+        fix_folder_stuff(b_data, cloud_folder, f_uid) 
+
         uid = boards_data[env]
         b_data['dashboard']['uid'] = uid 
         b_data['dashboard']['id'] = None
         b_data['overwrite'] = True
-        (f_id, f_uid) = FOLDERS[boards_data['folder']][env]
-        b_data['folderUid'] = f_uid
-        b_data['folderId'] = f_id
         b_data['message'] = "Updated to sync with prod"
 
         # prod alerts do not apply to other environments
@@ -109,6 +136,5 @@ for (name, boards_data) in BOARDS.items():
         with open("/tmp/dash", 'w') as dashfile:
            json.dump(b_data, dashfile, indent=4)
         print('---')
-        token = tokens[env]
-        os.system(POST_CMD.format(token, env)) 
+        os.system(POST_CMD.format(cloud_token, env)) 
 
