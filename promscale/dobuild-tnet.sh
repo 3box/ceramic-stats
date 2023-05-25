e49872b343fe811e72e2cfb202815eae1a4a0df4
Golda Velez <golda@3box.io>
2023-05-24 17:10:17 -0700

    correct task def image names

--- promscale/dobuild-tnet.sh
+++ promscale/dobuild-tnet.sh
@@ -1,4 +1,4 @@
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 967314784947.dkr.ecr.us-east-2.amazonaws.com
-docker build -t ceramic-tnet-promscale .
-docker tag ceramic-tnet-promscale:latest 967314784947.dkr.ecr.us-east-2.amazonaws.com/ceramic-tnet-promscale:tnet
-docker push 967314784947.dkr.ecr.us-east-2.amazonaws.com/ceramic-tnet-promscale:tnet
+docker build -t ceramic-tnet-grafana-promscale .
+docker tag ceramic-tnet-grafana-promscale:latest 967314784947.dkr.ecr.us-east-2.amazonaws.com/ceramic-tnet-grafana-promscale:tnet
+docker push 967314784947.dkr.ecr.us-east-2.amazonaws.com/ceramic-tnet-grafana-promscale:tnet
